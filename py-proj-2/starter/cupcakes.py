8bd1979de86f5cacf0cba1d6863e09d349455159
Juan Torres Reyes <juandtorres23@yahoo.com>
2022-11-19 15:35:28 -0500

    parts 1-5 completed, on to adding css

--- py-proj-2/starter/cupcakes.py
+++ py-proj-2/starter/cupcakes.py
@@ -79,11 +79,16 @@ cupcake1.add_sprinkles("Red", "White", "Blue")
 cupcake2 = Mini("Oreo", .99, "Chocolate", "Cookies and Cream")
 cupcake2.add_sprinkles("Oreo pieces")
 cupcake3 = Large("Red Velvet", 3.99, "Red Velvet", "Cream Cheese")
+cupcake4 = Regular("Guava King", 3.20, "Vanilla", "Guava paste")
+cupcake5 = Large("Cheese Cake", 4.20, "Vanilla", "Creame Cheese")
+cupcake5.add_sprinkles("Pie crust")
 
 cupcake_list = [
     cupcake1,
     cupcake2,
-    cupcake3
+    cupcake3,
+    cupcake4,
+    cupcake5
 ]
 
 
@@ -100,16 +105,61 @@ def write_new_csv(file, cupcakes):
             else:
                 writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
 
-write_new_csv("sample.csv", cupcake_list)
 
-def add_cupcake(file, cupcake):
-    with open(file, 'a', newline="\n") as csvfile:
-        fieldnames = ["size", "name", "price","flavor", "frosting", "sprinkles", "filling"]
+
+# def add_cupcake(file, cupcake):
+#     with open(file, 'a', newline="\n") as csvfile:
+#         fieldnames = ["size", "name", "price","flavor", "frosting", "sprinkles", "filling"]
+#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+#         if hasattr(cupcake, "filling"):
+#             writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "filling":cupcake.filling, "sprinkles": cupcake.sprinkles})
+#         else:
+#             writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
+
+def get_cupcakes(file):
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        reader = list(reader)
+        return reader
+
+def find_cupcake(file, name):
+    for cupcake in get_cupcakes(file):
+        if cupcake["name"] == name:
+            return cupcake
+    return None
+
+def add_cupcake_dictionary(file, cupcake):
+    with open(file, "a", newline="\n") as csvfile:
+        fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(cupcake)
 
-        if hasattr(cupcake, "filling"):
-            writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "filling":cupcake.filling, "sprinkles": cupcake.sprinkles})
-        else:
-            writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
 
-print(cupcake1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# write_new_csv("cupcakes.csv", cupcake_list)
+
+# This was a call to create the order.csv file
+
+# with open("order.csv", "w", newline="\n") as csvfile:
+#         fieldnames = ["size", "name", "price","flavor", "frosting", "sprinkles", "filling"]
+#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+#         writer.writeheader()
--- py-proj-2/starter/server.py
+++ py-proj-2/starter/server.py
@@ -1,22 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
+from cupcakes import get_cupcakes, find_cupcake, add_cupcake_dictionary
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", cupcakes = get_cupcakes("cupcakes.csv"))
 
 @app.route("/cupcakes")
 def all_cupcakes():
     return render_template("cupcakes.html")
 
-@app.route("/cupcake_individual")
-def individual_cupcake():
-    return render_template("individual_cupcake.html")
+@app.route("/add-cupcake/<name>")
+def add_cupcake(name):
+    cupcake = find_cupcake("cupcakes.csv", name)
+
+    if cupcake:
+        add_cupcake_dictionary("orders.csv", cupcake)
+        return redirect(url_for("home"))
+    else:
+        return "Sorry cupcake not found."
+
+@app.route("/individual_cupcake/<name>")
+def individual_cupcake(name):
+    cupcake = find_cupcake('cupcakes.csv', name)
+
+    return render_template("individual_cupcake.html", cupcake=cupcake)
+
 
 @app.route("/order")
 def order():
-    return render_template("order.html")
+    return render_template("order.html", cupcakes = get_cupcakes("orders.csv"))
 
 if __name__ == "__main__":
     # app.env = "development"
